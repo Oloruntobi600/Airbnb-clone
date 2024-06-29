39abd39604945edda46c95f95269f1032db17219
Oloruntobi600 <tobiajayi60@gmail.com>
2024-06-29 15:51:24 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
+const path = require('path');
+
 module.exports = {
-  "mode": "development", // or "production"
-  "output": {
-    "filename": "[name].pack.js"
+  mode: 'development', // or 'production'
+  entry: {
+    index: './index',
   },
-  "resolve": {
-    "extensions": [
-      ".js",
-      ".json"
-    ],
-    "alias": {}
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].pack.js',
   },
-  "module": {
-    "rules": [
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {},
+  },
+  module: {
+    rules: [
       {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ]
-          }
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
-        "exclude": /node_modules/,
-        "test": /\.js$/
-      }
-    ]
+      },
+    ],
   },
-  "entry": {
-    "index": "./index"
-  }
-}
+};
